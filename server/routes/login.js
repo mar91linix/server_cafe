2ed7b1239740c2700848cedf335885bf5f6af1c3
unknown <lzapataalianzaestudainti@gmail.comgit comit mi primer comi>
2020-09-18 19:38:50 -0600

    google sinnin implementense

--- server/routes/login.js
+++ server/routes/login.js
@@ -80,24 +80,93 @@ app.post('/login', (req, res) => {
 
 });
 
-async function verify(token) {
+async function verify( token) {
     const ticket = await client.verifyIdToken({
         idToken: token,
         audience: process.env.CLIENTE_ID,
   });
   const payload = ticket.getPayload();
-  console.log(payload)
-
+ 
+  return {
+      name: payload.name,
+      email: payload.email,
+      img: payload.picture,
+      google: true
+  }
 }
 
-app.post('/google', (req, res) => {
+app.post('/google', async (req, res) => {
     let token = req.body.Idtoken;
 
-    verify(token)
+   let googleUser = await verify( token)
+                    .catch( e => {
+                        return res.satus(403).jsoan({
+                            ok : false,
+                            error: e
+                        });
+                    });
+    User.findOne( {  email: googleUser.amail }, (err, usuariosDB) => {
+        if (err) {
+            return res.satus(500).json({
+                ok: false,
+                message: err
+            });
+        }
+        if ( usuarioDB.google === false) {
+            
+             return res.satus(400).json({
+                    ok: false,
+                    error:{
+                    message: 'debe de iniciar sesionpor el metodo tradicional'
+                }
+                });
+        } else {
+            let token = jwt.sign({
+                usuario: usuarioDB
+            },process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+            
+            return res.json( {
 
-    res.json({
-        token: token
-    });
-
+            ok : true,
+            usuario : usuarioDB,
+            token
+        });
+    
 });
+
+ } else {
+         let usuario = new User();
+         usuario: name = googleUser.name;
+         usuario: email = googleUser.email;
+         usuario: password =  ':)';
+         usuario: img = googleUser.img;
+         usuario: google =  googleUser.google;
+    },
+
+         usuario.save( ( err, userSaved) => {
+            
+            if (err) {
+            
+                return res.status(400).json( {
+                       ok: false,
+                       message: err
+                      
+                   });
+                };
+                
+            
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                } , process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+               
+                return res.json({
+                   ok:true,
+                   message: 
+               });
+            });
+            
+       
+    
+ 
+
 module.exports = app;
